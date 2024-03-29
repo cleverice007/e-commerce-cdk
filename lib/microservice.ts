10d897a10b1268c93d7fc22f4d2fea8d37f76ff2
cleverice007 <cleverice90128@gmail.com>
2024-03-29 12:56:13 +0800

    create lambda function createProductFunction

--- lib/microservice.ts
+++ lib/microservice.ts
@@ -9,3 +9,41 @@ interface MicroserviceProps {
     basketTable: ITable;
     orderTable: ITable;
 }
+
+export class Microservices extends Construct {
+    public readonly productMicroservice: NodejsFunction;
+    public readonly basketMicroservice: NodejsFunction;
+    public readonly orderingMicroservice: NodejsFunction;
+
+    constructor(scope: Construct, id: string, props: SwnMicroservicesProps){
+        super(scope, id);
+                
+        // product microservices
+        this.productMicroservice = this.createProductFunction(props.productTable);
+        // basket microservices
+        this.basketMicroservice = this.createBasketFunction(props.basketTable);
+        // ordering Microservice
+        this.orderingMicroservice = this.createOrderingFunction(props.orderTable);
+    }
+    private createProductFunction(productTable: ITable) : NodejsFunction {
+        const nodeJsFunctionProps: NodejsFunctionProps = {
+            bundling: {
+                externalModules: [
+                    'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+                ],
+            },      
+            environment: {
+                PRIMARY_KEY: 'id',
+                DYNAMODB_TABLE_NAME: productTable.tableName,
+            },
+            runtime: Runtime.NODEJS_14_X,
+        }
+    
+        const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
+            entry: join(__dirname, `/../src/product/index.js`),
+            ...nodeJsFunctionProps,
+        });
+            
+        productTable.grantReadWriteData(productFunction);
+        return productFunction;
+    }
