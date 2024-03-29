89978c9ad955700635857898ef568c5ea944584f
cleverice007 <cleverice90128@gmail.com>
2024-03-29 12:19:53 +0800

    create product,basket,order table & mircoservice

---
+++ lib/microservice.ts
@@ -0,0 +1,11 @@
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction, NodejsFunctionProps } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { join } from "path";
+
+interface MicroserviceProps {
+    productTable: ITable;
+    basketTable: ITable;
+    orderTable: ITable;
+}
