1e796683fe20f613eb007d8c226ef625e9e1080d
cleverice007 <cleverice90128@gmail.com>
2024-03-30 17:30:19 +0800

    create basket database in dynamodb

--- lib/database.ts
+++ lib/database.ts
@@ -3,23 +3,42 @@ import { AttributeType, BillingMode, ITable, Table } from "aws-cdk-lib/aws-dynam
 import { Construct } from "constructs";
 
 export class DynamoDB extends Construct {
-    public readonly productTable: ITable;
+  public readonly productTable: ITable;
+  public readonly basketTable: ITable;
 
-    constructor(scope: Construct, id: string){
-        super(scope, id);
-      
-        // Product DynamoDb Table Creation
-        const productTable = new Table(this, 'product', {
-            partitionKey: {
-              name: 'id',
-              type: AttributeType.STRING
-            },
-            tableName: 'product',
-            removalPolicy: RemovalPolicy.DESTROY,
-            billingMode: BillingMode.PAY_PER_REQUEST
-          });
 
-        this.productTable = productTable;
-    }
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    //product table
+    this.productTable = this.createProductTable();
+    //basket table
+    this.basketTable = this.createBasketTable();
+  }
+  private createProductTable(): ITable {
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+    return productTable;
+  }
+
+  private createBasketTable(): ITable {
+    const basketTable = new Table(this, 'basket', {
+      partitionKey: {
+        name: 'userName',
+        type: AttributeType.STRING,
+      },
+      tableName: 'basket',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+    return basketTable;
+  }
 
 }
