897cab57a4ebba9f67329522d027912fdc228e38
cleverice007 <cleverice90128@gmail.com>
2024-03-30 15:58:40 +0800

    debug

--- src/product/index.js
+++ src/product/index.js
@@ -1,3 +1,8 @@
+import { DeleteItemCommand, GetItemCommand, PutItemCommand, QueryCommand, ScanCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { ddbClient } from "./ddbClient";
+import { v4 as uuidv4 } from 'uuid';
+
 export async function handler(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
     
