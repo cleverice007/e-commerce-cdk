0af6510be8e16666dcc2ab3ba18a859afd12ad12
cleverice007 <cleverice90128@gmail.com>
2024-03-31 13:04:59 +0800

    create EventBusConstruct
    設定事件來源＆target

---
+++ lib/eventbus.ts
@@ -0,0 +1,39 @@
+import { EventBus, Rule } from "aws-cdk-lib/aws-events";
+import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
+import { IFunction } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+
+interface EventBusProps {
+    publisherFuntion: IFunction;
+    targetFuntion: IFunction;
+}
+
+export class EventBusConstruct extends Construct{
+   
+        constructor(scope: Construct, id: string, props: EventBusProps) {
+            super(scope, id);
+    
+            //eventbus
+            const bus = new EventBus(this, 'EventBus', {
+                eventBusName: 'EventBus'
+            });
+        
+            const checkoutBasketRule = new Rule(this, 'CheckoutBasketRule', {
+                eventBus: bus,
+                enabled: true,
+                description: 'When Basket microservice checkout the basket',
+                eventPattern: {
+                    source: ['com.basket.checkoutbasket'],
+                    detailType: ['CheckoutBasket']
+                },
+                ruleName: 'CheckoutBasketRule'
+            });
+        
+            // need to pass target to Ordering Lambda service
+            checkoutBasketRule.addTarget(new LambdaFunction(props.targetFuntion)); 
+            
+            bus.grantPutEventsTo(props.publisherFuntion);
+                // AccessDeniedException - is not authorized to perform: events:PutEvents
+    
+        }
+    }
