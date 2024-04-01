8cc27f3d1202fc081a7fab682022fa563ccd1671
cleverice007 <cleverice90128@gmail.com>
2024-04-01 12:31:01 +0800

    create create order function
    接收basketCheckoutEvent, 新增orde date 存入db

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -1,3 +1,9 @@
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { ddbClient } from "./ddbClient";
+
+
+
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
     return {
@@ -6,3 +12,28 @@ exports.handler = async function(event) {
       body: `Hello from Ordering ! You've hit ${event.path}\n`
     };	    
 };
+
+
+const createOrder = async (basketCheckoutEvent) => {
+  try {
+    console.log(`createOrder function. event : "${basketCheckoutEvent}"`);
+
+    // set orderDate for SK of order dynamodb
+    const orderDate = new Date().toISOString();
+    basketCheckoutEvent.orderDate = orderDate;
+    console.log(basketCheckoutEvent);
+    
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(basketCheckoutEvent || {})
+    };
+
+    const createResult = await ddbClient.send(new PutItemCommand(params));
+    console.log(createResult);
+    return createResult;
+
+  } catch(e) {
+    console.error(e);
+    throw e;
+  }
+}
